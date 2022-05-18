e3f191b70308f6f85ab5241a69543bddbb1241fe
Kerry-Cho <cdh0012@naver.com>
2022-05-19 00:11:13 +0900

    fix production webpack

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,4 +4,19 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'cheap-module-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.node$/,
+                loader: 'native-ext-loader',
+                options: {
+                    rewritePath: `${__dirname}/../build`,
+                },
+            },
+            {
+                test: /\.js$/,
+                loader: 'node-bindings-loader',
+            },
+        ],
+    },
 });
